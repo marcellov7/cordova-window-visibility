7fc58659a38cb504dec80492a2a6919da472407f
marcellov7 <marcelloviolini92@gmail.com>
2024-09-01 15:46:04 +0200

    up

--- src/android/CustomWebViewEngine.java
+++ src/android/CustomWebViewEngine.java
@@ -6,13 +6,20 @@ import android.view.View;
 import org.apache.cordova.engine.SystemWebViewEngine;
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.ConfigXmlParser;
 
 public class CustomWebViewEngine extends SystemWebViewEngine {
-    public CustomWebViewEngine(Context context, CordovaPreferences preferences) {
-        super(new CustomWebView(context), preferences);
+    public CustomWebViewEngine(Context context) {
+        this(new CustomWebView(context));
     }
 
     public CustomWebViewEngine(CustomWebView webView) {
         super(webView);
+    }
+
+    public void init(CordovaWebView parentWebView, CordovaInterface cordova, Client client,
+                     ConfigXmlParser configXmlParser) {
+        super.init(parentWebView, cordova, client, configXmlParser);
+        // Qui puoi aggiungere eventuali inizializzazioni personalizzate se necessario
     }
 }
