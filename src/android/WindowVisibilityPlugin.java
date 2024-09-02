b113aad1bf2685887276edfa55b2d6d91d654468
marcellov7 <marcelloviolini92@gmail.com>
2024-09-02 10:17:27 +0200

    up

--- src/android/WindowVisibilityPlugin.java
+++ src/android/WindowVisibilityPlugin.java
@@ -10,17 +10,23 @@ import org.json.JSONException;
 
 public class WindowVisibilityPlugin extends CordovaPlugin {
     private CordovaWebView webView;
+    private int visibilityDelay;
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         this.webView = webView;
+        this.visibilityDelay = preferences.getInteger("VISIBILITY_DELAY", 1000);
     }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("enable")) {
             this.enable(callbackContext);
+            return true;
+        } else if (action.equals("setDelay")) {
+            int delay = args.getInt(0);
+            this.setDelay(delay, callbackContext);
             return true;
         }
         return false;
@@ -34,14 +40,18 @@ public class WindowVisibilityPlugin extends CordovaPlugin {
         });
     }
 
+    private void setDelay(int delay, CallbackContext callbackContext) {
+        this.visibilityDelay = delay;
+        callbackContext.success("Delay set to " + delay + " ms");
+    }
+
     @Override
     public void onStop() {
         super.onStop();
-        // Wake up sleeping beauty one second after Maleficent (WebView) has put her to sleep
         cordova.getThreadPool().execute(new Runnable() {
             public void run() {
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(visibilityDelay);
                     webView.getEngine().getView().dispatchWindowVisibilityChanged(View.VISIBLE);
                 } catch (InterruptedException e) {
                     // do nothing
--- www/windowVisibility.js
+++ www/windowVisibility.js
@@ -3,6 +3,9 @@ var exec = require('cordova/exec');
 var WindowVisibility = {
     enable: function(success, error) {
         exec(success, error, "WindowVisibility", "enable", []);
+    },
+    setDelay: function(delay, success, error) {
+        exec(success, error, "WindowVisibility", "setDelay", [delay]);
     }
 };
 
